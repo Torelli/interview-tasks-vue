721fa339d269ffe721ce873e53943466180825c6
Giovanni Walter <giovanni.walter@outlook.com>
2025-05-07 21:35:03 -0300

    fix: fixed bugs and improved code by implementing Vue Reactivity API

--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,47 +1,46 @@
 <script setup lang="ts">
-const numbers = [];
-let limit = 100;
+import { computed, ref, watch } from 'vue';
 
-function n()
-{
-	let numbers = [];
-	for(var i = 0; i < limit; i++) { numbers = [...numbers, i]; }
+const limit = ref(100);
+const randomNumbersFromLimit = computed(() => {
+	const numbers = [];
+	for (let i = 0; i < limit.value; i++) {
+		numbers.push(i);
+	}
+	return numbers.sort(() => Math.random() - 0.5)
+})
+const nums = ref([] as HTMLElement[])
+const hoveredNumber = ref(0)
 
-	return numbers.sort(() => Math.random() - 0.5);
-}
+watch(hoveredNumber, () => {
+	if (hoveredNumber.value !== 0) {
+		for (let i = 0; i < nums.value.length; i++) {
+			for (let i = 0; i < nums.value.length; i++) {
+				const num = Number(nums.value[i].textContent?.trim());
+				if (hoveredNumber.value % num === 0) {
+					nums.value[i].classList.add('active')
+				} else {
+					nums.value[i].classList.remove('active')
+				}
+			}
+		}
+	}
+	else {
+		nums.value.forEach(elem => elem.classList.remove('active'))
+	}
+})
 
-function hov(number) {
-  const nums = document.querySelectorAll('.number');
-
-  for(let i = 0; i < nums.length; i++)
-  {
-    const num = nums[i].textContent.trim();
-    if(number % num === 0)
-    {
-      nums[i].classList.add('active')
-      console.log('divisor', num)
-    }
-  }
-}
-
-function reset()
-{
-	const nums = document.querySelectorAll('.number');
-	nums.forEach(num => num.classList.remove('active'))
+function reset() {
+	hoveredNumber.value = 0;
 }
 </script>
 
 <template>
 	<div>
 		<input type="number" v-model="limit" /><br /><br />
-		<div class="number"
-			:id="'number-'+number"
-			v-for="number in n()"
-			:key="number"
-			@mouseover="hov(number)"
-			@mouseout="reset"
-		>
-			{{ number }}
+		<div class="number" v-for="num in randomNumbersFromLimit" :key="num"
+			@mouseover="hoveredNumber = Number(num)" @mouseout="reset" ref="nums">
+			{{ num }}
 		</div>
 	</div>
 </template>
